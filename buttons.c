be78338e3738cd65c799be322ed4b03bc6be03d8
Krzysztof Szczesniak <k.szczesniak@althink.com>
2015-10-23 21:24:46 +0200

    #8 Improve buttons response time and eliminate fake long presses

--- buttons.c
+++ buttons.c
@@ -18,20 +18,35 @@ static app_button_cfg_t buttons[] =
 
 static const uint8_t BUTTONS_NO = sizeof(buttons)/sizeof(buttons[0]);
 
+static uint32_t long_press_handled = 0;
+
 static void button_handler(uint8_t pin_no, uint8_t button_action) {
     uint32_t err_code;
 	
-	  if(APP_BUTTON_PUSH == button_action){
-		
+	  if (APP_BUTTON_PUSH == button_action){
+			long_press_handled &= ~(1<<pin_no);
 		  err_code = app_timer_start(m_button_long_press_timer_id,
                                BUTTON_LONG_PRESS_DELAY,
-                               (void *)(pin_no));
+                               (void *)((uint32_t)pin_no));
       if (err_code != NRF_SUCCESS)
       {
           // The impact in app_button of the app_timer queue running full is losing a button press.
           // The current implementation ensures that the system will continue working as normal. 
       }
-   }
+   } else {
+			if (!(long_press_handled & (1<<pin_no))) {
+					for (uint32_t i = 0; i < BUTTONS_NO; i++)
+					{
+							app_button_cfg_t * p_btn = &buttons[i];
+
+							if (pin_no == p_btn->pin_no)
+							{
+									app_timer_stop(m_button_long_press_timer_id);
+									scr_mngr_handle_event(SCR_EVENT_BUTTON_PRESSED, 1<<i);
+							}
+					}
+			}
+	 }
 }
 
 static void button_long_press_timeout_handler(void * p_context) {
@@ -52,9 +67,10 @@ static void button_long_press_timeout_handler(void * p_context) {
 						}
 							
 						if (pushed) {
+								// button is still pushed so this is a long press
+								// mark this pin as used so normal handler that will be invoked after button release will not trigger event
+								long_press_handled |= 1<<pin_no;
 								scr_mngr_handle_event(SCR_EVENT_BUTTON_LONG_PRESSED, 1<<i);
-						} else {
-								scr_mngr_handle_event(SCR_EVENT_BUTTON_PRESSED, 1<<i);
 						}
 				}
 	  }
