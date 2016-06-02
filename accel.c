20a7f85cacb377d8aed1549e7af7608a946d62ca
Pavel Vasilyev <pkvasilyev@gmail.com>
2016-06-02 02:08:11 +0200

    Tune pedometer parameters

--- accel.c
+++ accel.c
@@ -17,22 +17,22 @@
 #include "config.h"
 
 #define DEFAULT_I2C_ADDR	0x1D
-#define FIFO_PACKET_LEN		6  // 6 vectors fit one 20 byte message
-#define FIFO_BYTES				3  // x+y+z
+#define FIFO_PACKET_LEN		6	// 6 vectors fit one 20 byte message
+#define FIFO_BYTES				3	// x+y+z
 #define FIFO_PACKET_SIZE	FIFO_PACKET_LEN*FIFO_BYTES
-#define FIFO_LENGTH				30  // take FIFO length divisible by packet length
+#define FIFO_LENGTH				30	// take FIFO length divisible by packet length
 #define FIFO_PACKETS			CEIL(FIFO_LENGTH,FIFO_PACKET_LEN)
 #define FIFO_SIZE					FIFO_BYTES*FIFO_LENGTH
 
-#define SLEEP_SAMPLE_COUNT				125  // number of stats samples for 10 seconds
+#define SLEEP_SAMPLE_COUNT				125	// number of stats samples for 10 seconds
 #define SLEEP_MAX_BATCH_SIZE			12
-#define SLEEP_BYTES								3  // min+max+avg
+#define SLEEP_BYTES								3	// min+max+avg
 
 #define PED_AMP_MIN								30
 #define PED_AMP_MAX								120
 #define PED_DELAY_MIN							10
 #define PED_DELAY_MAX							20
-#define PED_DEBOUNCE_THRESHOLD		20
+#define PED_DEBOUNCE_THRESHOLD		5
 #define PED_WAIT_MAX							0x01
 #define PED_WALKING								0x02
 
@@ -114,7 +114,7 @@ void sleep_set_batch_size(uint8_t size) {
 	sleep_check_batch_queue();
 }
 
-static void accel_process_step(bool count) {
+static inline void accel_process_step(bool count) {
 	if (count) {
 		if (ped_mode & PED_WALKING) {
 			steps++;
@@ -127,7 +127,7 @@ static void accel_process_step(bool count) {
 			}
 		}
 		ped_mode ^= PED_WAIT_MAX;
-	} else {
+	} else if (steps_debounce > 0) {
 		steps_debounce--;
 		if (steps_debounce == 0) {
 			ped_mode &= ~PED_WALKING;
@@ -136,32 +136,27 @@ static void accel_process_step(bool count) {
 }
 
 static void accel_int_handler(void * p_event_data, uint16_t event_size) {
-		uint8_t int_src;
-		uint8_t int_detail;
-		accel_read_register(0x0C, &int_src);
-//		const char hex_str[]= "0123456789ABCDEF";
-//		char value[5] = "0x00\0";
+	uint8_t int_src;
+	uint8_t int_detail;
+	accel_read_register(0x0C, &int_src);
+//	const char hex_str[]= "0123456789ABCDEF";
+//	char value[5] = "0x00\0";
 #ifdef DEBUG
-		value[2] = hex_str[int_src >> 4];
-		value[3] = hex_str[int_src & 0xf];
-		mlcd_draw_text(value, 15, 15, MLCD_XRES, NULL, FONT_NORMAL_BOLD, 0);
+	value[2] = hex_str[int_src >> 4];
+	value[3] = hex_str[int_src & 0xf];
+	mlcd_draw_text(value, 15, 15, MLCD_XRES, NULL, FONT_NORMAL_BOLD, 0);
 #endif
 
-		if (int_src & 0x10) {
-			accel_read_register(0x10, &int_detail);
-#ifdef DEBUG
-			value[2] = hex_str[int_detail >> 4];
-			value[3] = hex_str[int_detail & 0xf];
-			mlcd_draw_text(value, 15, 35, MLCD_XRES, NULL, FONT_NORMAL_BOLD, 0);
-#endif
-			// tilt event
-			if (orientation == 0x87 && int_detail == 0x85) {
-				scr_mngr_handle_event(SCR_EVENT_WRIST_SHAKE, 0);
-				orientation = 0;
-			}
-			if ((int_detail & 0x40) == 0) // if not in z-lockout zone
-				orientation = int_detail;
+	if (int_src & 0x10) {
+		accel_read_register(0x10, &int_detail);
+		// tilt event
+		if (orientation == 0x87 && int_detail == 0x85) {
+			scr_mngr_handle_event(SCR_EVENT_WRIST_SHAKE, 0);
+			orientation = 0;
 		}
+		if ((int_detail & 0x40) == 0) // if not in z-lockout zone
+			orientation = int_detail;
+	}
 		
 	// FIFO
 	if (int_src & 0x40) {
@@ -195,82 +190,79 @@ static void accel_int_handler(void * p_event_data, uint16_t event_size) {
 						
 						if (acc_ped) {
 							int direction = SIGN(dy);
-							if ((ped_mode & PED_WALKING) || steps_debounce > 0) {
+							if (steps_debounce > 0) {
 								peak_min_count++;
 								peak_max_count++;
 								next_max_count++;
 								next_min_count++;
-							}
-							// up peak
-							if (old_dir >= 0 && direction < 0) {
-								// if not in walking mode start at any peak
-								if ((ped_mode & PED_WALKING) == 0 && steps_debounce == 0) {
-//									if (acc - peak_min > PED_AMP_MIN) {
-										steps_debounce++;
-										ped_mode &= ~PED_WAIT_MAX;
-										peak_max = acc;
-										peak_max_count = 0;
-										next_max = -128;
-										next_max_count = 0;
-//									}
-								} else if (peak_max_count < PED_DELAY_MIN) {
-									// restart searching if better peak found
-									if (acc > peak_max) {
-										peak_max = acc;
-										peak_max_count = 0;
-										next_max = -128;
+								// up peak
+								if (old_dir >= 0 && direction < 0) {
+									// if not in walking mode start at any peak
+									if (peak_max_count < PED_DELAY_MIN) {
+										// restart searching if better peak found
+										if (acc > peak_max) {
+											peak_max = acc;
+											peak_max_count = 0;
+											next_max = -128;
+											next_max_count = 0;
+										}
+									} else if (peak_max_count <= PED_DELAY_MAX && acc > next_max) {
+										// choose the max in the window
+										next_max = acc;
 										next_max_count = 0;
 									}
-								} else if (peak_max_count <= PED_DELAY_MAX && acc > next_max) {
-									// choose the max in the window
-									next_max = acc;
+								}
+								// down peak
+								if (old_dir <= 0 && direction > 0) {
+									// if not in walking mode start at any peak
+									if (peak_min_count < PED_DELAY_MIN) {
+										// restart searching if better peak found
+										if (acc < peak_min) {
+											peak_min = acc;
+											peak_min_count = 0;
+											next_min = 127;
+											next_min_count = 0;
+										}
+									} else if (peak_min_count <= PED_DELAY_MAX && acc < next_min) {
+										// choose the min in the window
+										next_min = acc;
+										next_min_count = 0;
+									}	
+								}
+								
+								// out of max window
+								if (peak_max_count > PED_DELAY_MAX) {
+									accel_process_step(ped_mode & PED_WAIT_MAX && next_max - peak_min > PED_AMP_MIN);
+									peak_max = next_max;
+									peak_max_count = next_max_count;
+									next_max = -128;
 									next_max_count = 0;
 								}
-							}
-							// down peak
-							if (old_dir <= 0 && direction > 0) {
-								// if not in walking mode start at any peak
-								if ((ped_mode & PED_WALKING) == 0 && steps_debounce == 0) {
-//									if (peak_max - acc > PED_AMP_MIN) {
-										steps_debounce++;
-										ped_mode |= PED_WAIT_MAX;
-										peak_min = acc;
-										peak_min_count = 0;
-										next_min = 127;
-										next_min_count = 0;
-//									}
-								} else if (peak_min_count < PED_DELAY_MIN) {
-									// restart searching if better peak found
-									if (acc < peak_min) {
-										peak_min = acc;
-										peak_min_count = 0;
-										next_min = 127;
-										next_min_count = 0;
-									}
-								} else if (peak_min_count <= PED_DELAY_MAX && acc < next_min) {
-									// choose the min in the window
-									next_min = acc;
+								// out of min window
+								if (peak_min_count > PED_DELAY_MAX) {
+									accel_process_step((ped_mode & PED_WAIT_MAX) == 0 && peak_max - next_min > PED_AMP_MIN);
+									peak_min = next_min;
+									peak_min_count = next_min_count;
+									next_min = 127;
 									next_min_count = 0;
-								}	
-							}
-							
-							if ((ped_mode & PED_WALKING) || steps_debounce > 0) {							
-							// out of max window
-							if (peak_max_count > PED_DELAY_MAX) {
-								accel_process_step(ped_mode & PED_WAIT_MAX && next_max - peak_min > PED_AMP_MIN);
-								peak_max = next_max;
-								peak_max_count = next_max_count;
-								next_max = -128;
-								next_max_count = 0;
-							}
-							// out of min window
-							if (peak_min_count > PED_DELAY_MAX) {
-								accel_process_step((ped_mode & PED_WAIT_MAX) == 0 && peak_max - next_min > PED_AMP_MIN);
-								peak_min = next_min;
-								peak_min_count = next_min_count;
-								next_min = 127;
-								next_min_count = 0;
-							}
+								}
+							} else { // debounce counter == 0
+								if (old_dir >= 0 && direction < 0) {
+									steps_debounce++;
+									ped_mode &= ~PED_WAIT_MAX;
+									peak_max = acc;
+									peak_max_count = 0;
+									next_max = -128;
+									next_max_count = 0;
+								}
+								if (old_dir <= 0 && direction > 0) {
+									steps_debounce++;
+									ped_mode |= PED_WAIT_MAX;
+									peak_min = acc;
+									peak_min_count = 0;
+									next_min = 127;
+									next_min_count = 0;
+								}
 							}
 							
 							old_dir = direction;
@@ -279,6 +271,13 @@ static void accel_int_handler(void * p_event_data, uint16_t event_size) {
 								data[pos-3] = peak_max;
 								data[pos-1] = peak_min;
 							}
+//		value[2] = hex_str[steps_debounce >> 4];
+//		value[3] = hex_str[steps_debounce & 0xf];
+//		mlcd_draw_text(value, 50, 0, MLCD_XRES, NULL, FONT_NORMAL_BOLD, 0);
+//		value[2] = hex_str[ped_mode >> 4];
+//		value[3] = hex_str[ped_mode & 0xf];
+//		mlcd_draw_text(value, 50, 20, MLCD_XRES, NULL, FONT_NORMAL_BOLD, 0);
+//		mlcd_fb_flush();
 						}
 
 						if (acc_sleep) {
@@ -308,31 +307,23 @@ static void accel_event_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t 
 	APP_ERROR_CHECK(err_code);
 }
 
-//void accel_get_values(int8_t * x, int8_t * y, int8_t * z) {
-//	int8_t data[3];
-//	accel_read_multi_register(0x01, (uint8_t*)data, 3);
-//	*x = data[0];
-//	*y = data[1];
-//	*z = data[2];
-//}
-
 static uint32_t accel_int_init(uint8_t pin_no) {
-    uint32_t err_code;
-    if (!nrf_drv_gpiote_is_init()) {
-        err_code = nrf_drv_gpiote_init();
-        if (err_code != NRF_SUCCESS) {
-            return err_code;
-        }
-    }
-    nrf_drv_gpiote_in_config_t config = GPIOTE_CONFIG_IN_SENSE_HITOLO(false);
-    config.pull = NRF_GPIO_PIN_PULLUP;
+	uint32_t err_code;
+	if (!nrf_drv_gpiote_is_init()) {
+		err_code = nrf_drv_gpiote_init();
+		if (err_code != NRF_SUCCESS) {
+			return err_code;
+		}
+	}
+	nrf_drv_gpiote_in_config_t config = GPIOTE_CONFIG_IN_SENSE_HITOLO(false);
+	config.pull = NRF_GPIO_PIN_PULLUP;
 
-    err_code = nrf_drv_gpiote_in_init(pin_no, &config, accel_event_handler);
-    if (err_code != NRF_SUCCESS) {
-        return err_code;
-    }
-    nrf_drv_gpiote_in_event_enable(pin_no, true);
-		return NRF_SUCCESS;
+	err_code = nrf_drv_gpiote_in_init(pin_no, &config, accel_event_handler);
+	if (err_code != NRF_SUCCESS) {
+		return err_code;
+	}
+	nrf_drv_gpiote_in_event_enable(pin_no, true);
+	return NRF_SUCCESS;
 }
 
 static void accel_fifo_init() {
@@ -383,70 +374,72 @@ static void accel_tilt_init() {
 //}
 
 void accel_interrupts_reset() {
-		// 0x2A: CTRL_REG1 System Control 1 register
-		// go to standby mode
-		accel_write_register(0x2A, 0);
+	// 0x2A: CTRL_REG1 System Control 1 register
+	// go to standby mode
+	accel_write_register(0x2A, 0);
 
-		// 0x0E: XYZ_DATA_CFG register
-		// HPF_OUT - disable high pass filter
-		// FS - 2g
-		accel_write_register(0x0E, 0x00);
+	// 0x0E: XYZ_DATA_CFG register
+	// HPF_OUT - disable high pass filter
+	// FS - 2g
+	accel_write_register(0x0E, 0x00);
 
-		// 0x2B: CTRL_REG2 System Control 2 register
-		// set Low Noise Low Power mode
-		accel_write_register(0x2B, 0x09);
-		// set Low Power mode
-		//accel_write_register(0x2B, 0x1B);
+	// 0x2B: CTRL_REG2 System Control 2 register
+	// set Low Noise Low Power mode
+	accel_write_register(0x2B, 0x09);
+	// set Low Power mode
+	//accel_write_register(0x2B, 0x1B);
 
-		// 0x2C: CTRL_REG3 Interrupt Control register
-		//accel_write_register(0x2C, 0x30);//0x40
+	// 0x2C: CTRL_REG3 Interrupt Control register
+	//accel_write_register(0x2C, 0x30);//0x40
 
-		uint8_t interrupt = 0;
-		if (get_settings(CONFIG_ACCELEROMETER) || get_settings(CONFIG_SLEEP_AS_ANDROID || get_settings(CONFIG_PEDOMETER))) {
-			reset_sleep();
-			accel_fifo_init();
-			interrupt |= 0x40;
-		}
-		accel_tilt_init();
-		interrupt |= 0x10;
-		//accel_motion_init();
-		//accel_transient_init();
-		//accel_pulse_init();
-		
-		// 0x2D: CTRL_REG4 Interrupt Enable register (Read/Write)
-		// transient+orientation+pulse+freefall
-		//accel_write_register(0x2D, 0x3c);
-		// FIFO + orientation
-		accel_write_register(0x2D, interrupt);
+	uint8_t interrupt = 0;
+	if (get_settings(CONFIG_ACCELEROMETER) || get_settings(CONFIG_SLEEP_AS_ANDROID || get_settings(CONFIG_PEDOMETER))) {
+		reset_sleep();
+		accel_fifo_init();
+		interrupt |= 0x40;
+	}
+	accel_tilt_init();
+	interrupt |= 0x10;
+	//accel_motion_init();
+	//accel_transient_init();
+	//accel_pulse_init();
+	
+	// 0x2D: CTRL_REG4 Interrupt Enable register (Read/Write)
+	// transient+orientation+pulse+freefall
+	//accel_write_register(0x2D, 0x3c);
+	// FIFO + orientation
+	accel_write_register(0x2D, interrupt);
 
-		// 0x0F: HP_FILTER_CUTOFF 
-		accel_write_register(0x0F, 0);
-		
-		// 0x29: ASLP_COUNT 
-		//accel_write_register(0x29, 0x1);
+	// 0x0F: HP_FILTER_CUTOFF 
+	accel_write_register(0x0F, 0);
+	
+	// 0x29: ASLP_COUNT 
+	//accel_write_register(0x29, 0x1);
 
-		// go to active mode
-		// 0x2A: CTRL_REG1 System Control 1 register
-		// ASLP_RATE 01 = DR 101 = 12,5Hz
-		accel_write_register(0x2A, 0x69);
-		// ASLP_RATE 10 = DR 110 = 6,25Hz
-		//accel_write_register(0x2A, 0xB1);
-		// ASLP_RATE 00 = DR 100 = 50Hz
-		// accel_write_register(0x2A, 0x23);
+	// go to active mode
+	// 0x2A: CTRL_REG1 System Control 1 register
+	// ASLP_RATE 01 = DR 101 = 12,5Hz
+	accel_write_register(0x2A, 0x69);
+	// ASLP_RATE 10 = DR 110 = 6,25Hz
+	//accel_write_register(0x2A, 0xB1);
+	// ASLP_RATE 00 = DR 100 = 50Hz
+	// accel_write_register(0x2A, 0x23);
 	next_min = 127;
 	next_max = -128;
 	ped_mode = 0;
 	steps_debounce = 0;
+	peak_max_count = 0;
+	peak_min_count = 0;
 }
 
 void accel_init(void) {
 	twi_master_init();
 	
-  uint32_t err_code;
+	uint32_t err_code;
 	// err_code = accel_int_init(ACCEL_INT1);
-  // APP_ERROR_CHECK(err_code);
+	// APP_ERROR_CHECK(err_code);
 	err_code = accel_int_init(ACCEL_INT2);
-  APP_ERROR_CHECK(err_code);
+	APP_ERROR_CHECK(err_code);
 	// 0x2B: CTRL_REG2 System Control 2 register
 	// reset
 	accel_write_register(0x2B, 0x40);
@@ -456,16 +449,16 @@ void accel_init(void) {
 }
 
 void accel_write_register(uint8_t reg, uint8_t value) {
-		uint8_t data[] = {reg, value};
-		twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, data, 2, true);
+	uint8_t data[] = {reg, value};
+	twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, data, 2, true);
 }
 
 void accel_read_register(uint8_t reg, uint8_t* value) {
-		twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, &reg, 1, false);
-		twi_master_transfer	(	(DEFAULT_I2C_ADDR<<1)|TWI_READ_BIT, value, 1, true);
+	twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, &reg, 1, false);
+	twi_master_transfer	(	(DEFAULT_I2C_ADDR<<1)|TWI_READ_BIT, value, 1, true);
 }
 
 void accel_read_multi_register(uint8_t reg, uint8_t* value, uint8_t length) {
-		twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, &reg, 1, false);
-		twi_master_transfer	(	(DEFAULT_I2C_ADDR<<1)|TWI_READ_BIT, value, length, true);
+	twi_master_transfer	(	DEFAULT_I2C_ADDR<<1, &reg, 1, false);
+	twi_master_transfer	(	(DEFAULT_I2C_ADDR<<1)|TWI_READ_BIT, value, length, true);
 }
